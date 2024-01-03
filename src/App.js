bc34c30f3dd0180d73b30cb3a45da4e6d9acca8c
a.fouda <a.fouda@crocoit.com>
2024-01-04 00:29:07 +0200

    a.fouda : main | create first component with add two ways to create component (functional / class)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// functional component
+// function App(){
+//     const myStyle={textAlign:"center", backgroundColor:"yellow"};
+//     return (
+//         <div style={myStyle}>
+//             <p >Hello World</p>
+//             <p >Hello World</p>
+//         </div>
+//     );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+
+
+// class component
+class App extends React.Component{
+    render(){
+    const myStyle={textAlign:"center", backgroundColor:"yellow"};
+        return (
+                <div style={myStyle}>
+                    <p >Hello World</p>
+                    <p >Hello World</p>
+                </div>
+            );
+        }
 }
 
 export default App;
