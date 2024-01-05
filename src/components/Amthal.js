cc834bd6389c4370712a4804bb8d97a3acb69dee
a.fouda <a.fouda@crocoit.com>
2024-01-05 23:47:00 +0200

    a.fouda : main | create component Amthal and install library react-router-dom and using routes

---
+++ src/components/Amthal.js
@@ -0,0 +1,11 @@
+import React , {Component} from "react";
+
+class Amthal extends Component{
+    render(){
+        return(
+            <h2>Amthal</h2>
+        );
+    }
+}
+
+export default Amthal;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import  ReactDOM  from "react-dom";
 import App from "./components/App";
-ReactDOM.render(<App />,document.getElementById('root'));
+import { BrowserRouter ,Routes , Route } from "react-router-dom";
+import Amthal from "./components/Amthal";
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<App />}/>
+            <Route path="/amthal" element={<Amthal />}/>
+        </Routes>
+    </BrowserRouter>
+    ,document.getElementById('root'));
