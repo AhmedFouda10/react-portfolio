55786d635d74b05e67461f7b282f9a4933d3fb10
a.fouda <a.fouda@crocoit.com>
2024-01-06 19:21:21 +0200

    a.fouda : pro-6 | create component Gallery

---
+++ src/components/Gallery.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import IMAGES from '../data/images';
+import ImageShadow from 'react-image-shadow';
+import "react-image-shadow/assets/index.css";
+
+class Gallery extends Component {
+    render() {
+        return (
+            <div>
+                {
+                    IMAGES.map(imageObject =>{
+                        return <Image src={imageObject.image} description={imageObject.description}/>
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+class Image extends Component{
+    render(){
+        const src =this.props.src;
+        const description =this.props.description;
+        return(
+            <div className='gallery'>
+                <a target="_blank" href={src} rel="noreferrer">
+                    <ImageShadow src={src} />
+                </a>
+                <p>{description}</p>
+            </div>
+        );
+    }
+}
+
+export default Gallery;
---
+++ src/data/images.js
@@ -0,0 +1,75 @@
+import img1 from "../images/1.jpg";
+import img2 from "../images/2.jpg";
+import img3 from "../images/3.jpg";
+import img4 from "../images/4.jpg";
+import img5 from "../images/5.jpg";
+import img6 from "../images/6.jpg";
+import img7 from "../images/7.jpg";
+import img8 from "../images/8.jpg";
+import img9 from "../images/9.jpg";
+import img10 from "../images/10.jpg";
+
+const IMAGES=[
+    {
+        id :1,
+        title : "img1",
+        description : "img1 description",
+        image :img1 
+    },
+    {
+        id :2,
+        title : "img2",
+        description : "img2 description",
+        image :img2 
+    },
+    {
+        id :3,
+        title : "img3",
+        description : "img3 description",
+        image :img3 
+    },
+    {
+        id :4,
+        title : "img4",
+        description : "img4 description",
+        image :img4 
+    },
+    {
+        id :5,
+        title : "img5",
+        description : "img5 description",
+        image :img5 
+    },
+    {
+        id :6,
+        title : "img6",
+        description : "img6 description",
+        image :img6 
+    },
+    {
+        id :7,
+        title : "img7",
+        description : "img7 description",
+        image :img7 
+    },
+    {
+        id :8,
+        title : "img8",
+        description : "img8 description",
+        image :img8 
+    },
+    {
+        id :9,
+        title : "img9",
+        description : "img9 description",
+        image :img9 
+    },
+    {
+        id :10,
+        title : "img10",
+        description : "img10 description",
+        image :img10
+    }
+];
+
+export default IMAGES;
