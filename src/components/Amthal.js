f7147f1dd538d0e7e15af90c19e444bbcae02cbb
a.fouda <a.fouda@crocoit.com>
2024-01-06 18:39:20 +0200

    a.fouda | main : return random mathal and return amthal tem

--- src/components/Amthal.js
+++ src/components/Amthal.js
@@ -1,9 +1,42 @@
 import React , {Component} from "react";
 
 class Amthal extends Component{
+    constructor(){
+        super();
+        this.state={mathal : {} ,amthal : []};
+        this.fetchAmthal =this.fetchAmthal.bind(this);
+    }
+    componentDidMount(){
+        fetch("http://localhost:3005/amthal/random")
+            .then((response) => response.json())
+            .then((json) => this.setState({mathal : json}));
+    }
+    async fetchAmthal(){
+        try {
+            const response = await fetch("http://localhost:3005/amthal/ten")
+            .then((response) => response.json())
+            .then((json) => this.setState({amthal : json}));
+            
+            console.log(response)           
+            
+        } catch (error) {
+            console.log(error)           
+        }
+    }
     render(){
         return(
-            <h2>Amthal</h2>
+            <div>
+                <h2>Amthal</h2>
+                <p>{this.state.mathal.title}</p>
+                <hr/>
+                <h3>you need more from amthals?</h3>
+                <button className="btn" onClick={this.fetchAmthal}>Amthal</button>
+                {
+                    this.state.amthal.map(mathal =>{
+                        return <p>{mathal.title}</p>
+                    })
+                }
+            </div>
         );
     }
 }
